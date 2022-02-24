85c6af162b709564b44a1113b2cd563d5512fc54
Jafarov Murad <m.jafarov@itstep.edu.az>
2022-02-24 20:50:49 +0400

    First Commit

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            var connectionId = Context.ConnectionId;
+            Clients.All.SendAsync("OnConnected", "Salam Dostlar");
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", message);
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
